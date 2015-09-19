e72260d5f48d438719789aef3a3d1b365a629064
Shyam Rajendran <rshyam.psg@gmail.com>
2015-09-18 19:11:09 -0500

    submitted

--- shyam/noise.m
+++ shyam/noise.m
@@ -1,25 +1,10 @@
-function [output] = noise(x,ratio)
-output = zeros(x);
-scale = 0.02;
-xx = (x+1)/2;
-index = 2;
-for i = 1:xx
-    k = 0;
-    index = 0;
-    for j = 1:xx
-        if (i==1 || i==x || j==1 || j==x)
-            continue;
-        else
-            if (j<=4 && i<=4)
-                output(i,j) = (ratio+i+k) * scale / 3500;
-            else  
-            end
-        end
-        k = k+10;
-    end
-end
-topright = output*gen2(x,1);
-tophalf = output + topright ;
-bottomhalf = flipud(tophalf);
-output = tophalf + bottomhalf;
+function [output] = noise(x, scale)
+output =[   0       0       0       0       0       0       0;
+            0       0        0.0250      0.0500      0.0250      0       0;
+            0       0.0250      0.1000      0.1500      0.1000      0.0250      0;
+            0       0.0500      0.1500      0.2000      0.1500      0.0500      0;
+            0       0.0250      0.1000      0.1500      0.1000      0.0250      0;
+            0       0       0.0250      0.0500      0.0250      0       0;
+            0       0       0       0       0       0       0 ];
+output = output .* scale;
 end
