3601781e6bc7801807ebfe674e57bad8a5fc805e
saikatroyc <rychwdh2@illinois.edu>
2015-11-03 00:18:38 -0600

    code on video images

--- project/test_code/test_rotation.m
+++ project/test_code/test_rotation.m
@@ -8,35 +8,38 @@ G_LOW = 9999
 G_HIGH = -1
 B_LOW = 9999
 B_HIGH = -1
-for count = 1:4
-    im = imread(['../testhand/testimg/saikat/CALIB_', int2str(count), '.JPG']);
-    [row, col, h] = size(im);
-    for i = 1:row
-        for j = 1:col
-            if im(i,j,1) < R_LOW
-                R_LOW = im(i,j,1);
-            end
-            if im(i,j,2) < G_LOW
-                G_LOW = im(i,j,2);
-            end
-            if im(i,j,3) < B_LOW
-                B_LOW = im(i,j,3);
-            end
-            if im(i,j,1) > R_HIGH
-                R_HIGH = im(i,j,1);
-            end
-            if im(i,j,2) > G_HIGH
-                G_HIGH = im(i,j,2);
-            end
-            if im(i,j,3) > B_HIGH
-                B_HIGH = im(i,j,3);
+for count = 1:7
+    if exist(['../testhand/testimg/saikat_1/CALIB_', int2str(count), '.JPG'], 'file')
+        im = imread(['../testhand/testimg/saikat_1/CALIB_', int2str(count), '.JPG']);
+        [row, col, h] = size(im);
+        for i = 1:row
+            for j = 1:col
+                if im(i,j,1) < R_LOW
+                    R_LOW = im(i,j,1);
+                end
+                if im(i,j,2) < G_LOW
+                    G_LOW = im(i,j,2);
+                end
+                if im(i,j,3) < B_LOW
+                    B_LOW = im(i,j,3);
+                end
+                if im(i,j,1) > R_HIGH
+                    R_HIGH = im(i,j,1);
+                end
+                if im(i,j,2) > G_HIGH
+                    G_HIGH = im(i,j,2);
+                end
+                if im(i,j,3) > B_HIGH
+                    B_HIGH = im(i,j,3);
+                end
             end
         end
-    end    
+    end
 end
 
 for count = 1:10
-    im = imread(['../testhand/testimg/saikat/IMG_', int2str(count), '.JPG']);
+    if exist(['../testhand/testimg/saikat_1/IMG_', int2str(count), '.JPG'], 'file')
+    im = imread(['../testhand/testimg/saikat_1/IMG_', int2str(count), '.JPG']);
     figure,
     subplot(3,3,1)
     
@@ -189,6 +192,7 @@ for count = 1:10
     colormap gray
     imshow(e)
     figure, plot(dist)
+    end
 end
 
 
