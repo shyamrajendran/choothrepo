abd3cea80322b630f8cd3cca1073ded28fb99a37
Shyam Rajendran <rshyam.psg@gmail.com>
2015-12-03 13:05:59 -0600

    recommit shit for ms

--- project/demoCode/grab_periodic.m
+++ project/demoCode/grab_periodic.m
@@ -2,15 +2,17 @@ function [sub_list] = grab_periodic(alist, window_size,index)
 i = 1;
 ind = 1;
 len = length(alist);
+sub_list = cell(len,1);
 while (i < len) 
     if (index == 0) 
-        sub_list(1,ind) = round(alist(i),0);
+        sub_list{ind} = num2str(alist(i));
         i = i + window_size;
     else
         i = i + window_size;
-        sub_list(1,ind) = round(alist(min(i,len)),0);
+        sub_list{ind} = num2str(alist(min(i,len)));
         
     end    
     ind = ind + 1;
 end
+%sub_list
 end
